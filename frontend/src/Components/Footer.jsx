152cbb064116793636c02b3e2790819ba2be2cfc
Tarun Ruttala <tarunruttala76@gmail.com>
2024-08-26 23:56:00 +0530

    second commit

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='items-center justify-center'>
+        <hr className='border-[1px] border-black'/>
+        <h1 className='text-center py-3 text-sm'>Copyright © 2024 - All right reserved by ACME Industries Ltd</h1>
+    </div>
+    </>
+  )
+}
+
+export default Footer
